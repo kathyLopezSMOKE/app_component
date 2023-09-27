dbce06284587921f9de284c9a44dcb6c88f97d8f
kathyLopezSMOKE <Katherine Lopez>
2023-09-27 08:10:45 -0500

    items

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,29 +8,84 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: 200,
-              width: 200,
-              decoration:  BoxDecoration(
-                color: Colors.amber,
-                image: DecorationImage(
-                  image: NetworkImage(
-                    "https://malditopaparazzo.com.ar/wp-content/uploads/2021/05/David-Chicle-768x770.jpg"),
-                ),
-                borderRadius: BorderRadius.circular(20.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    offset: Offset(5, 5),
-                    blurRadius: 12.0,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+             const SizedBox(height: 20,),
+              Container(
+                height: 200,
+                width: 200,
+                decoration:  BoxDecoration(
+                  color: Colors.amber,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://malditopaparazzo.com.ar/wp-content/uploads/2021/05/David-Chicle-768x770.jpg"),
                   ),
-                ],
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      offset: Offset(5, 5),
+                      blurRadius: 12.0,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 20,),            
+              Text("Flutter Components",
+              style: GoogleFonts.poppins(
+                fontSize: 20.0,
+                letterSpacing: 1,
+              ),
+              ),
+              SizedBox(
+                width: 160.0,
+                child: Divider(),
+              ),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class ItemComponentWidget extends StatelessWidget {
+  const ItemComponentWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 12.0,
+        vertical: 5.0,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15.0),
+        boxShadow: [
+          BoxShadow(     
+            color: Colors.black.withOpacity(0.5),
+            offset: Offset(5, 5),
+            blurRadius: 12.0,
+          ),
+        ],
+      ),
+      child: ListTile(
+        title: Text("Avatar",style: GoogleFonts.poppins(),),
+        subtitle: Text("Ir a detalles de Alert"),
+        leading: Icon(Icons.check_circle_outline),
+        trailing: Icon(Icons.chevron_right),
       ),
     );
   }
