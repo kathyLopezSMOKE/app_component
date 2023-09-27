ddc879be37ade340748e6149e7e1d8e4d7b4ceff
kathyLopezSMOKE <Katherine Lopez>
2023-09-27 07:16:27 -0500

    imagen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              decoration:  BoxDecoration(
+                color: Colors.amber,
+                image: DecorationImage(
+                  image: NetworkImage(
+                    "https://malditopaparazzo.com.ar/wp-content/uploads/2021/05/David-Chicle-768x770.jpg"),
+                ),
+                borderRadius: BorderRadius.circular(20.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    offset: Offset(5, 5),
+                    blurRadius: 12.0,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
