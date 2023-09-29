a5b102ef5abb6c4e667db9c35747857dc9a8499a
kathyLopezSMOKE <Katherine Lopez>
2023-09-29 07:50:25 -0500

    alert

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
-  const AlertPage({super.key});
+  
+  showMyAlert(BuildContext context1){
+    showDialog(
+      context: context1, 
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text("Titulo del alert 1"),
+          content: Text("Este es el contenido del alert 1"),
+          backgroundColor: Colors.white,
+          actions: [
+            /*
+            Text("Si"),
+            ElevatedButton(onPressed: (){},child: Text("No"),),
+            IconButton(onPressed: (){}, icon: Icon(Icons.cancel),color: Colors.red,),
+            */
+            TextButton(onPressed: (){}, child: Text("Aceptar")),
+            TextButton(
+              onPressed: (){
+                Navigator.pop(context);
+
+              }, 
+              child: Text(
+                "Cancelar",
+                style: TextStyle(color: Colors.redAccent),
+              ),
+            ),
+          ],
+        );
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Alert Page"),
+        backgroundColor: Colors.black45,
       ),
-      );
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: (){
+              showMyAlert(context);
+            },
+            child: Text("Alert 1"),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -6,7 +6,32 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Avatar Page"),),
+      appBar: AppBar(
+        title: Text("Avatar Page"),
+        centerTitle: true,
+        elevation: 10.0, //sombra
+        backgroundColor: Colors.amberAccent,
+        actions: [
+          CircleAvatar(
+            backgroundImage: NetworkImage("https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg"),
+          ),
+          IconButton(
+            onPressed: (){}, 
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          FadeInImage(
+            placeholder: AssetImage("assets/images/loading.gif"), 
+            image: AssetImage("assets/images/David-Chicle.jpg"),
+            height: 300.0,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 1600),
+          ),
+        ],
+      ),
     );
   }
 }
